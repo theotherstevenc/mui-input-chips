e13fbe81c513ca4a7f9dc2a8dbb1f3bda20eb047
Steven Collins <s.collins@salesforce.com>
2025-06-22 09:05:07 -0700

    feat: initialize mui-input-chips component library
    - Add package.json for project metadata and scripts
    - Create rollup.config.js for building the library
    - Implement InputChips component with styled chips and input field
    - Add index.ts for exporting InputChips and its props
    - Configure TypeScript with tsconfig.json for strict type checking

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import typescript from 'rollup-plugin-typescript2'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    typescript({
+      rollupCommonJSResolveHack: true,
+      clean: true,
+    }),
+  ],
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { default as InputChips } from './InputChips'
+export type { InputChipsProps } from './InputChips'
